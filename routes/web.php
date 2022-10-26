a4e8af9192e35c6b45dfeafbfa0c3df62ba7093f
Hamza Nawaz <hamza123nawaz@hotmail.com>
2022-10-26 15:51:14 +0100

    errors

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VisitorController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\LogoutController;
 Route::get('/', [WelcomeController::class, 'index']);
 Route::resource('/visitors', VisitorController::class);
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
-Route::get('/', [LogoutController::class, 'logout'])->name('logout');
+Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
 
