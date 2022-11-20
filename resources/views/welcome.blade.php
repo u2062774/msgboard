b0cb5d412c40e2d5dfd3df45626dac34764e2056
Hamza Nawaz <hamza123nawaz@hotmail.com>
2022-11-20 15:57:06 +0000

    add latest visitors to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 @extends('layouts.app')
-
-@section('title', 'Welcome to the sign it visitor book')
+@section('title', 'Latest visitors')
+@section('content')
+    @foreach ($latestVisitors as $visitor)
+        <article>
+            <h3><a href="{{ route('visitors.show', $visitor->id) }}">{{ $visitor->user->name }}</a></h3>
+            <p>{{ $visitor->comments }}</p>
+        </article>
+    @endforeach
+@endsection
